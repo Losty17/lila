4f70c4782e483f128d7adecb26d060cf51f0a24c
Vinícius Kappke <viniciuskappke@gmail.com>
2022-07-03 22:22:45 -0300

    docs

--- src/main/java/tech/kappke/lila/bot/utils/ObjectUtils.java
+++ src/main/java/tech/kappke/lila/bot/utils/ObjectUtils.java
@@ -1,9 +1,25 @@
 package tech.kappke.lila.bot.utils;
 
+/**
+ * This class defines many utilities for working with objects,
+ * such as getting improved readability and performance.
+ * 
+ * @author ytsol
+ * @since 0.0.1
+ */
 public class ObjectUtils
 {
-	public static <T> T getIfNotNull(T src, T other)
+	/**
+	 * This method provides a way to improve code readability when trying to
+	 * get an object when the first one was null
+	 * 
+	 * @param <T> implicit type of the provided objects
+	 * @param obj the main object
+	 * @param other the other object
+	 * @return the not null object
+	 */
+	public static <T> T getIfNotNull(T obj, T other)
 	{
-		return src != null ? src : other;
+		return obj != null ? obj : other;
 	}	
 }
